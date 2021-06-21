14b2afa8e7f09ef55f2d81a5f35e0b90d6e51bad
HoangCuongZitga <86188741+HoangCuongZitga@users.noreply.github.com>
2021-06-21 17:38:35 +0700

    Update UnityBasicUI.cs

--- UnityBasicUI.cs
+++ UnityBasicUI.cs
@@ -1,5 +1,17 @@
+// auto fill list while Editor has changed
 private void OnValidate()
     {
-        // auto fill list while Editor has changed
         if (listItem == null || listItem.Length == 0) listItem = transform.GetComponentsInChildren<SubData>();
+    }
+
+// load specific something by using Resource
+ public void SetSprite(string id)
+    {
+        imageOfItem.sprite = Resources.Load("ImageOfItems/" + id, typeof(Sprite)) as Sprite;
+    }
+
+//load all images from a path into list
+void LoadImages()
+    {
+        listSprites = Resources.LoadAll("ImageOfItems", typeof(Sprite)).Cast<Sprite>().ToList();
     }
